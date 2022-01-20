7748fa9ba5b62a47ef6603e5a5ec0e321a07828b
Mohammad javad zandiyeh <58389811+JavadZandiyeh@users.noreply.github.com>
2022-01-20 19:40:38 +0330

    check for QUANTUM and curTickNo

--- trap.c
+++ trap.c
@@ -13,6 +13,7 @@ struct gatedesc idt[256];
 extern uint vectors[];  // in vectors.S: array of 256 entry pointers
 struct spinlock tickslock;
 uint ticks;
+extern int increase_clockTickNo(void);
 
 void
 tvinit(void)
@@ -102,8 +103,9 @@ trap(struct trapframe *tf)
 
   // Force process to give up CPU on clock tick.
   // If interrupts were on while locks held, would need to check nlock.
+  int curTickNo = increase_clockTickNo();
   if(myproc() && myproc()->state == RUNNING &&
-     tf->trapno == T_IRQ0+IRQ_TIMER)
+     tf->trapno == T_IRQ0+IRQ_TIMER && curTickNo == QUANTUM)
     yield();
 
   // Check if the process has been killed since we yielded
